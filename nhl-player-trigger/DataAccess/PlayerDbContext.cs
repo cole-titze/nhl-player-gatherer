b66a66fa2905d09513c4bb659f2f64d75df21b47
colet <cole.titze@mines.sdsmt.edu>
2022-05-13 17:35:06 -0600

    Added Ef and models

---
+++ nhl-player-trigger/DataAccess/PlayerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class PlayerDbContext : DbContext
+    {
+        public DbSet<PlayerValue> PlayerValues { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=Players;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ nhl-player-trigger/DataAccess/PlayerValue.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess
+{
+    public class PlayerValue
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int value { get; set; }
+    }
+}
